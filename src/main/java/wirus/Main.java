d50235fd42f3151add467e6782cad03f81f40730
Michu <mich1992@wp.pl>
2018-09-09 18:05:30 +0200

    add icons & more

--- src/main/java/wirus/Main.java
+++ src/main/java/wirus/Main.java
@@ -7,10 +7,13 @@ import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.DialogEvent;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
-import java.awt.*;
+import java.awt.Dimension;
+import java.awt.Toolkit;
 import java.util.Random;
 
 public class Main extends Application {
@@ -36,7 +39,13 @@ public class Main extends Application {
         alert.setTitle("UMC UMC UMC");
         alert.setX(posX);
         alert.setY(posY);
-        alert.setHeaderText("HACKED!");
+        alert.setHeaderText("HAHAHA");
+        //Image image = new Image(getClass().getResourceAsStream("/iconResized2.png"));
+        Image image = new Image(getClass().getResourceAsStream("/hacked.png"));
+        ImageView imageView = new ImageView(image);
+        alert.setGraphic(imageView);
+        ((Stage)alert.getDialogPane().getScene().getWindow()).getIcons().add(new Image("/obama.jpg"));
+
         alert.setOnCloseRequest(new EventHandler<DialogEvent>() {
             public void handle(DialogEvent event) {
                 for (int i = 0; i < 10; i++) {
